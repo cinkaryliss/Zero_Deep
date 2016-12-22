4b87737a7b87b8c95f23844bf14e965627044865
Yoshi <cinkaryliss@gmail.com>
2016-12-22 10:35:47 +0900

    活性化関数の作成

---
+++ section3/step.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int) #xの内正のものをTrue,負のものをFalseにして、それらを1,0のそれぞれにしたものを格納
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) #y軸の範囲を指定
+plt.show()
